24056dcf33e78143e87fde359e37094daf488449
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-04-07 20:27:17 +0000

    made changes to stg models and yml

--- models/staging/nutrisense/stg_nutrisense__new_members.sql
+++ models/staging/nutrisense/stg_nutrisense__new_members.sql
@@ -1,6 +1,21 @@
-with source as (
+with
+
+source as (
 
     select * from {{ source('nutrisense', 'new_members') }}
+),
+
+renamed as (
+    select
+        customer_id,
+        charge_id,
+        date as charge_date,
+        cohort_date,
+        cohort_tag,
+        cohort_plan_commitment,
+        cohort_plan_commitment_ends,
+        cohort_plan_id
+    from source
 )
 
 select * from source
