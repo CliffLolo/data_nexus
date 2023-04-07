a5656345a9e0fb89110a7d244d7c1470852f6601
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-04-07 23:47:44 +0000

    restructure models dirs and made changes to stg models

--- models/staging/nutrisense/stg_nutrisense__active_members.sql
+++ models/staging/nutrisense/stg_nutrisense__active_members.sql
@@ -1,8 +1,27 @@
-with source as (
+with
+source as (
 
     select * from {{ source('nutrisense', 'active_members') }}
+),
+
+renamed as (
+
+    select
+        date as active_date,
+        first_charge_at,
+        customer_id,
+        cohort_date,
+        cohort_plan_commitment
+    from source
+),
+
+final as (
+
+    select * from renamed
 )
 
-select * from source
+select * from final
+
+
 
 
